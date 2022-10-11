188bb0941d98535a10b9ab76de25a93841441bec
samanthamarberger <slatham04@gmail.com>
2022-10-11 15:49:22 -0600

    commit tree 1cf421c8e078b70fec7d019e3aabe922193650b0

--- main.js
+++ main.js
@@ -3,6 +3,64 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heartBtn  = document.getElementsByClassName('like-glyph');
+
+function heartTapped(event) {
+  const btn = event.target;
+  mimicServerCall()
+  .then(function(serverMessage){
+    //alert(serverMessage);
+    if (btn.innerText === EMPTY_HEART) {
+      btn.innerText = FULL_HEART;
+      btn.className = 'activated-heart';
+    }
+    else {
+      btn.innerText = EMPTY_HEART;
+      //btn.className = '';
+      btn.classList.remove('activated-heart');
+    }
+  })
+  .catch(function(error) {
+    //alert(error.message);
+
+    const regularHeader = document.getElementById("modal")
+    regularHeader.classList.remove('hidden');
+    regularHeader.innerText = error;
+    console.log(error);
+    setTimeout(() => regularHeader.className = 'hidden', 3000);
+
+
+    /*const errorMessage = document.createElement('div');
+    errorMessage.innerHTML = '<div id="modal"><h2>Error!</h2><p id="modal-message></p></div>';
+    regularHeader.parentNode.replaceChild(errorMessage, regularHeader);
+    setTimeout(errorMessage.parentNode.replaceChild(regularHeader, errorMessage, 3000));
+    */
+  });
+}
+
+for (const button of heartBtn) {
+  button.addEventListener ("click", heartTapped);
+}
+
+/*for (button of heartBtn) {
+  button.addEventListener('click', function() {
+    mimicServerCall()
+    .then(function (data){
+      let heart = document.querySelector(`<span class="like-glyph>">'♡'</span>`);
+      const newItem = document.createElement('li');
+      newItem.innerHTML = `<span class="like-glyph">'♥'</span>`;
+      heart.parentNode.replaceChild(newItem, heart);
+    })
+    .catch(function(e) {
+      alert(e.message);
+
+      const regularHeader = document.getElementById("modal")
+      const errorMessage = document.createElement('div');
+      errorMessage.innerHTML = '<div id="modal"><h2>Error!</h2><p id="modal-message></p></div>';
+      regularHeader.parentNode.replaceChild(errorMessage, regularHeader);
+    })
+  });
+}*/
 
 
 
